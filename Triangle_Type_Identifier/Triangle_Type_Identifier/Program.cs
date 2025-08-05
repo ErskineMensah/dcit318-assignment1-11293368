5ca1f176d1f4ee3fac76064e6c7707f1a5dee603
Mensah <emensah669@st.ug.edu.gh>
2025-08-05 11:39:34 +0000

    Setting up project and folder for the triangle identifier console app

---
+++ Triangle_Type_Identifier/Triangle_Type_Identifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Type_Identifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
